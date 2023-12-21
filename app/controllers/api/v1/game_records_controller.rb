fd21c90a10af4b2b59330b801447bb9b5d444a24
Joe King <j.king92@gmail.com>
2023-12-21 15:05:14 -0700

    Create first endpoint for posting a game record with associated model and controller

---
+++ app/controllers/api/v1/game_records_controller.rb
@@ -0,0 +1,17 @@
+module Api
+  module V1 
+    class GameRecordsController < ApplicationController
+      def create
+        gr = GameRecord.create!(gr_params)
+
+        render status: :created
+      end
+
+      private
+
+      def gr_params
+        params.permit(:game_type, :player, :starting_score, :number_of_darts)
+      end
+    end
+  end
+end
